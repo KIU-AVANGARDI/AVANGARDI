438c211f98404521b8b29029f12bcb1fce813a3e
samKHa92 <samkha@gfmstudio.com>
2022-08-07 16:05:53 +0400

    Fixed minor bugs in backend

--- avangardiapi/kitchen/urls.py
+++ avangardiapi/kitchen/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('kitchen',views.KitchenView.as_view(),name="kitchenList"),
-    path('kitchen/search',views.SearchKitchenView.as_view(),name="kitchenSearch"),
-    path('kitchen/<int:pk>',views.KitchenView.as_view(),name="kitchenDetails")
+    path('products',views.KitchenView.as_view(),name="kitchenList"),
+    path('products/search',views.SearchKitchenView.as_view(),name="kitchenSearch"),
+    path('products/<int:pk>',views.KitchenView.as_view(),name="kitchenDetails")
 ]
--- avangardiapi/material/urls.py
+++ avangardiapi/material/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('material',views.MaterialView.as_view(),name="materialList"),
-    path('material/search',views.SearchMaterialView.as_view(),name="materialSearch"),
-    path('material/<int:pk>',views.MaterialView.as_view(),name="materialDetails"),
+    path('products',views.MaterialView.as_view(),name="materialList"),
+    path('products/search',views.SearchMaterialView.as_view(),name="materialSearch"),
+    path('products/<int:pk>',views.MaterialView.as_view(),name="materialDetails"),
 ]
