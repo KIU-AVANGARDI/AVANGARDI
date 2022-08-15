ce225341aa472669ebe5a5ef036144f5da0565e7
DoomedAndria <103760839+DoomedAndria@users.noreply.github.com>
2022-08-15 13:05:14 +0400

    fixed navbar

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,33 +1,39 @@
-import React, { useState } from "react"
+import React, {useRef, useState} from "react"
 import logo from "../logo.svg"
 import "../styles/Navbar.css"
 
 
 export default function Navbar (props){
-    const [opened,setOpened] = useState(true)
+    const [opened,setOpened] = useState(false)
+    const navRef = useRef()
+    function showLinks(){
+        navRef.current.classList.toggle("show-nav")
+    }
+
     return(
-        <nav>
+        <nav >
             <div className="logo-container">
                 <img src={logo} alt={"logo"}/>
             </div>
+            <div  ref={navRef} className="links-cont">
+                <div className="links">
+                    <ul>
+                        <li><a href="/">მთავარი</a></li>
+                        <li><a href="#">მაღაზია</a></li>
+                        <li><a href="#">ვაკანსია</a></li>
+                        <li><a href="#">ჩვენს შესახებ</a></li>
+                    </ul>
+                </div>
+                <div className="login-button">
+                    <button className="login">Log in</button>
+                </div>
+            </div>
 
-            <div className="links" style={{
-                display: opened ? 'block' : 'none'
-            }}>
-                <ul>
-                    <li><a href="#">მთავარი</a></li>
-                    <li><a href="#">მაღაზია</a></li>
-                    <li><a href="#">ვაკანსია</a></li>
-                    <li><a href="#">ჩვენს შესახებ</a></li>
-                </ul>
-            </div>
-            <div className="login-button" style={{
-                display: opened ? 'flex' : 'none'
-            }}>
-                <button className="login">Log in</button>
-            </div>
             <div className={opened?'hamburger is-active':'hamburger'}
-                 onClick={()=> setOpened(!opened)}>
+                 onClick={()=> {
+                     setOpened(!opened)
+                     showLinks()
+                 }}>
                 <span></span>
                 <span></span>
                 <span></span>
