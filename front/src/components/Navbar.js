f669892005d87a5fdc8ca8992c04e6adf0abcc40
DoomedAndria <103760839+DoomedAndria@users.noreply.github.com>
2022-08-24 03:39:56 +0400

    vacancy form email connected

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -52,9 +52,9 @@ export default function Navbar (props){
                         }><FontAwesomeIcon icon={faArrowRightToBracket}/> &nbsp;&nbsp;შესვლა</button>
                     </div>
                     <div className={loginActive?"login-dropdown active-login":"login-dropdown"}>
-                        <a href="/authorization"><p>ავტორიზაცია</p></a>
+                        <a style={!loginActive?{pointerEvents:"none"}:{}} href="/authorization"><p>ავტორიზაცია</p></a>
                         <hr/>
-                        <a href="/registration"><p>რეგისტრაცია</p></a>
+                        <a style={!loginActive?{pointerEvents:"none"}:{}} href="/registration"><p>რეგისტრაცია</p></a>
                     </div>
                 </div>
 
